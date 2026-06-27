6ca9ce8219b81b49771523bce63f6b064d0b9329
Iamshashankwagh <shashankwagh2@gmail.com>
2026-06-27 13:42:06 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/oden-recipe/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
